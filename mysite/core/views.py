9ed1de1d81d6816bee7b1be6ecb80ae3dac0db79
buij <buij@dnb.com>
2017-12-22 17:15:25 -0600

    Update json web token permission.

--- mysite/core/views.py
+++ mysite/core/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render
 from django.views import generic
 
+from rest_framework import status
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.views import Response
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from core.models import Profile
 
 from firebase_admin import auth
 
 
 @api_view(['GET'])
-@authentication_classes((SessionAuthentication, BasicAuthentication, TokenAuthentication))
+@authentication_classes((SessionAuthentication, BasicAuthentication, TokenAuthentication, JSONWebTokenAuthentication))
 @permission_classes((IsAuthenticated,))
 def get_firebase_minted_token(request, format=None):
     if request.user.is_authenticated():
@@ -21,10 +23,10 @@ def get_firebase_minted_token(request, format=None):
 
         # Return the minted key.
         return Response({
-            'firebase-token': auth.create_custom_token(uid=str(profile.uuid))
-        })
+            'token': auth.create_custom_token(uid=str(profile.uuid))
+        }, status=status.HTTP_200_OK)
 
     else:
         return Response({
-            'firebase-token': None
-        })
+            'token': None
+        }, status=status.HTTP_401_UNAUTHORIZED)
